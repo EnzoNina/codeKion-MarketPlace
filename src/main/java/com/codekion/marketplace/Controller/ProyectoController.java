48cd0e093f38c074bdbbe7904f73e636cd6d28c3
Enzo Josue Nina Aragon <U19221435@utp.edu.pe>
2024-01-29 19:12:40 -0500

    Cambios en las paginas HTML

--- src/main/java/com/codekion/marketplace/Controller/ProyectoController.java
+++ src/main/java/com/codekion/marketplace/Controller/ProyectoController.java
@@ -31,5 +31,10 @@ public class ProyectoController {
         return proyectosService.findByJefeProyecto(usuario.getId());
     }
 
+    @GetMapping("/BuscarProyectos")
+    public String buscarProyectos(Model model) {
+        return "pages/buscarProyectos";
+    }
+
 
 }
