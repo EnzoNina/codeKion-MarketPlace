d457ba28c46db16ad1b57e85ac21d55f7831c7b6
Enzo Nina <ninaenzo70@gmail.com>
2024-02-02 15:34:19 -0500

    Agregando .war

---
+++ out/artifacts/codeKion_war_exploded/WEB-INF/classes/static/js/buscarColaboradores.js
@@ -0,0 +1,33 @@
+// Espera a que el documento esté completamente cargado
+document.addEventListener("DOMContentLoaded", function () {
+    $('#myModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var user = button.data('user');
+        $('#userInput').val(user);
+    });
+});
+
+//Cargamos los proyectos con Ajax
+$.ajax({
+    url: "/getProyectosPorUsuario",
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+        // Actualiza el contenido del select con los proyectos
+        var proyectosSelect = $("#proyecto");
+        proyectosSelect.empty();
+
+        console.log(data);
+
+        if (data.length === 0) {
+            proyectosSelect.after("<p>No hay proyectos disponibles</p>")
+        } else {
+            $.each(data, function (index, proyecto) {
+                proyectosSelect.append("<option value='" + proyecto.id + "'>" + proyecto.nombreProyecto + "</option>");
+            });
+        }
+    },
+    error: function (error) {
+        console.error("Error al obtener proyectos:", error);
+    }
+});
